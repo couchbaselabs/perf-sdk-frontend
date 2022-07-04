143341c4cb4f452e5bc915993c0ed86e867364eb
Graham Pople <grahampople@gmail.com>
2022-07-04 18:17:15 +0100

    Java display

--- vue/src/components/Java.vue
+++ vue/src/components/Java.vue
@@ -11,7 +11,25 @@ export default {
   components: {Results},
   data() {
     return {
-      input:  {"inputs":[{"viewing":"cluster","params":[{"type":"unmanaged","memory":12000,"version":"7.1.0-2556-enterprise","cpuCount":4,"nodeCount":1}]}],"group_by":"impl.version","display":"duration_average_us","impl":{"language":"java"},"workload":{"description":null},"vars":{"doc_num":100000,"driverVersion":2,"horizontal_scaling":20},"graph_type":"Simplified","grouping_type":"Side-by-side"}
+      input: {
+        "inputs": [{
+          "viewing": "cluster",
+          "params": [{
+            "type": "unmanaged",
+            "memory": 12000,
+            "version": "7.1.0-2556-enterprise",
+            "cpuCount": 4,
+            "nodeCount": 1
+          }]
+        }],
+        "group_by": "impl.version",
+        "display": "duration_average_us",
+        "impl": {"language": "java"},
+        "workload": {"description": null},
+        "vars": {"doc_num": 100000, "driverVersion": 2, "horizontal_scaling": 20},
+        "graph_type": "Simplified",
+        "grouping_type": "Average"
+      }
     }
   }
 }
