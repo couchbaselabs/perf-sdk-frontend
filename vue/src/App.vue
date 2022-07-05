069f1679fbf035014581e12ed8d17be8ae2a1438
Graham Pople <grahampople@gmail.com>
2022-07-05 16:03:58 +0100

    Font

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -30,8 +30,10 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Lato&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Lato", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
