6ee3be5d326cfcffcc399e60ea716a91e0a07dbe
Graham Pople <grahampople@gmail.com>
2024-11-04 13:13:18 +0000

    Handle invalid semver versions
    E.g. C++'s "1.0.3-20241025.022106+960bdda"

--- nest/src/versions.ts
+++ nest/src/versions.ts
@@ -1,5 +1,15 @@
 const semver = require('semver');
 
 export function versionCompare(version1: string, version2: string) {
-    return semver.compare(version1, version2)
+    return semver.compare(normalizeVersion(version1), normalizeVersion(version2))
+}
+
+/**
+ * Normalizes a version string to make it compatible with semver.
+ * E.g. handles "1.0.3-20241025.022106+960bdda", where the ".0" is illegal.
+ * @param version - The version string to normalize.
+ * @returns The normalized version string.
+ */
+function normalizeVersion(version: string): string {
+    return version.replace(/\.0(\d{5})/, '-$1');
 }
